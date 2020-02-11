4dbbadc36fc07e52e6ea8c93d55b726196660775
Jan1616 <60182539+Jan1616@users.noreply.github.com>
2020-02-11 07:20:30 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -15,7 +15,10 @@ brick.buttonDown.onEvent(ButtonEvent.Pressed, function () {
 brick.buttonUp.onEvent(ButtonEvent.Pressed, function () {
     motors.largeBC.setInverted(true)
     pospesevanje(20)
-    motors.largeBC.steer(0, 20, 2.3, MoveUnit.Rotations)
+    motors.largeBC.steer(0, 20, 2.54, MoveUnit.Rotations)
+    //for (let i = 0; i < 4; i++) {
+
+    //}
     motors.stopAll()
 })
 
